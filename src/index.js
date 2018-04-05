edea97bd031709f39337ab519fdffd53428efb86
Gonçalo Freitas <goncalo.freitas@thought-creator.com>
2018-04-05 09:43:27 +0100

    HelloWorld

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+//import './style.sass';
+
+const App = () => (
+    <div>
+        <h1>Hello, world!</h1>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
